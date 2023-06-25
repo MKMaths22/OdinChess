ba7832698003bcaa581a39396da2e2e4487a517d
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-25 12:10:09 +0100

    #legal? to call #find_our_piece first

--- lib/move.rb
+++ lib/move.rb
@@ -4,23 +4,42 @@ class Move
 
 include Miscellaneous
 
-  def initialize
+  attr_accessor :string, :colour, :board, :our_piece
 
+  def initialize(string, colour, board)
+    @string = string
+    @colour = colour
+    @board = board
+    @our_piece = nil
+    @start_square = find_start_square
+    @finish_square = find_finish_square
   end
 
 # this #our_piece makes no sense yet! 
 
-def our_piece(move, colour, board)
-  possible_piece = board.string_to_square(move[0,2])
+  def legal?
+    our_piece = find_our_piece
+  end
+
+  def find_start_square
+    string_to_coords(string[0, 2])
+  end
+
+  def find_finish_square
+    string_to_coords(string[2, 2])
+  end
+
+  def find_our_piece
+    possible_piece = board.start_square
     unless possible_piece
       puts no_piece_error(move)
       return false
     end
-    unless possible_piece.colour = colour
+    unless possible_piece.colour == colour
       puts wrong_piece_error(move, colour)
       return false
     end
-  possible_piece  
-end
+    possible_piece  
+  end
 
 end
