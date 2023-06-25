b5da2dcc5d72c0e0743a420f6dbf4ea5a5a9d22e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-25 11:53:15 +0100

    Prepare to put methods into Move class

--- lib/move.rb
+++ lib/move.rb
@@ -4,4 +4,23 @@ class Move
 
 include Miscellaneous
 
+  def initialize
+
+  end
+
+# this #our_piece makes no sense yet! 
+
+def our_piece(move, colour, board)
+  possible_piece = board.string_to_square(move[0,2])
+    unless possible_piece
+      puts no_piece_error(move)
+      return false
+    end
+    unless possible_piece.colour = colour
+      puts wrong_piece_error(move, colour)
+      return false
+    end
+  possible_piece  
+end
+
 end
--- lib/player.rb
+++ lib/player.rb
@@ -10,8 +10,8 @@ class Player
   end
 
   def get_move(board)
-    proposed_move = gets.strip
-    move_to_play = all_valid?(proposed_move, @colour_moving, board)
+    maybe_move = gets.strip
+    move_to_play = all_valid?(maybe_move, @colour_moving, board)
     move_to_play ? move_to_play : get_move
     # move_to_play is either falsey if move not valid or it is a Move object
   end
@@ -34,34 +34,25 @@ class Player
   def all_valid?(maybe_move, colour, board)
     # maybe_move is the raw input from the player
     return false unless valid_input?(maybe_move)
+
+    this_move = Move.new(maybe_move, colour, board)
+    return this_move.legal?
+  end
     
-    moving_piece = our_piece(maybe_move, colour, board)
-    return false unless moving_piece  
+   # moving_piece = our_piece(maybe_move, colour, board)
+   # return false unless moving_piece  
     # our_piece returns the piece
     # can include string_to_coords in a module so this ValidMove class can use it directly
-    start_square = string_to_coords(maybe_move[0, 2])
+   # start_square = string_to_coords(maybe_move[0, 2])
     # start_square given in co-ordinates the board array can accept
-    final_square = string_to_coords(maybe_move[2, 2])
+    #final_square = string_to_coords(maybe_move[2, 2])
     
-    return false unless moving_piece.move_legal?(board, start_square, final_square)
+   # return false unless moving_piece.move_legal?(board, start_square, final_square)
     
     # The class of the piece takes care of the remaining tests because
     # type of piece dictates if move is possible and board class lets us
     # know if pieces in the way, Castling, En Passent possibilities 
 
-    return { 'start' => start_square, 'finish' => final_square }
-  end
-
-  def our_piece(move, colour, board)
-    possible_piece = board.string_to_square(move[0,2])
-      unless possible_piece
-        puts no_piece_error(move)
-        return false
-      end
-      unless possible_piece.colour = colour
-        puts wrong_piece_error(move, colour)
-        return false
-      end
-    possible_piece  
-  end
+    # return { 'start' => start_square, 'finish' => final_square }
+  # end
 end
