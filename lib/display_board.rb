c4d263e610d9b6e571581d5e235f267f457af2c2
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-07-24 17:19:05 +0100

    Simple refactoring and styling of DisplayBoard using Rubocop

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,27 +1,24 @@
 # frozen-string-literal: true
 
 # DisplayBoard class responsible for showing the board.
-# It displays so that the player whose turn it is to move sees their pawns going up the board,
-# as though the players are sat across a real-life board.
+# It displays so that the player whose turn it is to move sees their pawns going up
+# the board, as though the players are sat across a real-life board.
 class DisplayBoard
-
   require 'colorize'
 
   attr_accessor :colour
-  
+
   def initialize
     @colour = 'White'
   end
-  
-  EMPTY_STRING = '         '
 
-  FILE_LETTERS = *('a'..'h')
+  EMPTY_STRING = '         '
 
   def display_board(array)
     array = reverse_array(array) if @colour == 'Black'
     rank = 7
     until rank.negative?
-      (0..3).each do |num|
+      4.times do |num|
         puts make_a_row(array, rank, num)
       end
       rank -= 1
@@ -34,37 +31,36 @@ class DisplayBoard
   end
 
   def final_row
-    file_letters_to_use = @colour == 'White' ? FILE_LETTERS : FILE_LETTERS.reverse
+    file_letters = *('a'..'h')
+    file_letters_to_use = @colour == 'White' ? file_letters : file_letters.reverse
     string = EMPTY_STRING
     file_letters_to_use.each do |letter|
       string += "    #{letter}    "
     end
     string
   end
-  
+
   def make_a_row(array, rank, num)
     string = num == 2 ? number_string(rank) : EMPTY_STRING
-    (0..7).each do |file|
+    8.times do |file|
       poss_piece = array[file][rank]
       square_color = (rank + file).odd? ? :white : :light_black
-      if poss_piece
-        string += poss_piece.display_strings[num].colorize(background: square_color)
-      else
-        string += EMPTY_STRING.colorize(background: square_color)
-      end
+      string += if poss_piece
+                  poss_piece.display_strings[num].colorize(background: square_color)
+                else
+                  EMPTY_STRING.colorize(background: square_color)
+                end
     end
     string
   end
-  
+
   def reverse_array(array)
-    array.map { |rank| rank.reverse }.reverse
+    array.map(&:reverse).reverse
   end
-  
+
   def show_the_board(board)
-    # method asks the board class for the type and colour of pieces to display
-    # need to start with CarryingOutMove class which will have an object created after next_move has been shown to be legal in one_turn (Game class)
+    # self.colour set so that the board can display the correct way for each player.
     self.colour = board.colour_moving
     display_board(board.board_array)
   end
-
 end
