e4d65e6faa5975b5f3357d5fa47f9b5d6d78af73
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-07-24 15:13:03 +0100

    Make small style changes to game.rb in consultation with Rubocop

--- lib/game.rb
+++ lib/game.rb
@@ -3,32 +3,32 @@
 # The procedure for determining if a move is legal
 
 require 'yaml'
-require './board.rb'
-require './player.rb'
-require './display_board.rb'
-require './check_for_check.rb'
-require './result.rb'
-require './piece.rb'
-require './pawn.rb'
-require './other_pieces.rb'
-require './miscellaneous.rb'
-require './move.rb'
-require './change_the_board.rb'
-require './generate_legal_moves.rb'
+require './board'
+require './player'
+require './display_board'
+require './check_for_check'
+require './result'
+require './piece'
+require './pawn'
+require './other_pieces'
+require './miscellaneous'
+require './move'
+require './change_the_board'
+require './generate_legal_moves'
 
-# Game class takes care of overall progress of the game, asking players their names at the start. Also responsible for saving the game.  
+# Game class takes care of overall progress of the game, asking players their names at the start.
+# Also responsible for saving the game.
 # This is how Player objects are created and all of the other objects the Board object, which creates the Pieces.
 class Game
+  include Miscellaneous
 
-include Miscellaneous
-  
   attr_accessor :white, :black, :board, :result, :colour_moving, :legal_moves, :saved, :moving_name, :not_moving_name
-  
-  def initialize(board = Board.new, white = nil, black = nil, result = Result.new(make_the_hash(board.store_position)))
-    @board = board
-    @white = white
-    @black = black
-    @result = result
+
+  def initialize
+    @board = Board.new
+    @white = nil
+    @black = nil
+    @result = Result.new(make_the_hash(board.store_position))
     @colour_moving = 'White'
     @display_board = DisplayBoard.new
     @legal_moves = GenerateLegalMoves.new(board).find_all_legal_moves
@@ -36,18 +36,18 @@ include Miscellaneous
     @moving_name = nil
     @not_moving_name = nil
   end
-  
+
   def play_game
     create_the_players unless saved
-  
+    # when loading a saved game, do not create new players
     puts "It is #{colour_moving} to move." if saved
     self.saved = false
-    # if reloading a saved game, this variable must be changed 
+    # if reloading a saved game, this variable must be changed.
     turn_loop
   end
 
   def turn_loop
-      one_turn until result.game_over? || @saved
+    one_turn until result.game_over? || @saved
   end
 
   def one_turn
@@ -55,18 +55,24 @@ include Miscellaneous
     next_move = enter_move_or_save_game
     save_the_game if next_move == 'save'
     resign_the_game if next_move == 'resign'
-    # next_move is a either nil or a Move object which knows the input 'string' that started it from the Player, 'start_square', 'finish_square', 'colour', 'board' object, 'vector' (which is just subtract_vector(finish_square, start_square)), 'our_piece (the piece that is moving)', 'other_piece' which is nil unless it is a conventional capturing move, 'en_passent' which is Boolean (the only non-conventional capturing move) and 'castling' which is either false or gives the string of the form e.g. 'Black_0-0-0'
-    if next_move.kind_of?(Move)
-      boolean = next_move.pawn_move_or_capture?
-      player_of_the_move = next_move.colour == 'White' ? white : black 
-      ChangeTheBoard.new(next_move, board, player_of_the_move).update_the_board
-    # the #update_the_board method communicates with the move object next_move and the @board to get the board to update itself, including changing its @colour_moving. The
+    # next_move is a either a 'save' or 'resign' string or a Move object which knows the 'start_square',
+    # 'finish_square', 'colour', 'board' object, 'vector' (which is just subtract_vector(finish_square,
+    # start_square)), 'our_piece (the piece that is moving)', 'other_piece' which is nil unless it is
+    # a conventional capturing move, 'en_passent' which is Boolean (the only non-conventional capturing
+    # move) and 'castling' which is either false or gives the string of the form e.g. 'Black_0-0-0'
+    return unless next_move.is_a?(Move)
+
+    boolean = next_move.pawn_move_or_capture?
+    player_of_the_move = next_move.colour == 'White' ? white : black
+    ChangeTheBoard.new(next_move, board, player_of_the_move).update_the_board
+    # player needed in case they give input for pawn promotion
+    # the #update_the_board method communicates with the move object next_move and the
+    # @board to get the board to update itself, including changing its @colour_moving. The
     # @colour_moving in Game class gets toggled later
     # board.colour_moving is the next player
-      self.legal_moves = GenerateLegalMoves.new(board).find_all_legal_moves
-      consequences_of_move(boolean)
-      toggle_colours
-    end
+    self.legal_moves = GenerateLegalMoves.new(board).find_all_legal_moves
+    consequences_of_move(boolean)
+    toggle_colours
   end
 
   def update_moving_name
@@ -76,30 +82,34 @@ include Miscellaneous
   def update_not_moving_name
     self.not_moving_name = get_player_name_from_colour(other_colour(colour_moving))
   end
-  
+
   def resign_the_game
     result.declare_resignation(moving_name, not_moving_name)
   end
 
   def enter_move_or_save_game
     puts "Enter your move, #{moving_name}, in the format 'e4g6' for the starting square and finishing square, or type 'save' or 'resign' to save/resign the game."
-    next_move = @colour_moving == 'White' ? white.get_legal_move(board, legal_moves) : black.get_legal_move(board, legal_moves)
+    @colour_moving == 'White' ? white.get_legal_move(board, legal_moves) : black.get_legal_move(board, legal_moves)
   end
 
   def consequences_of_move(boolean)
     # boolean for whether the move was a pawn move or capture
     check_status = CheckForCheck.new(board.board_array, board.colour_moving).king_in_check?
-    
+
     mate_or_mate(check_status, result) unless legal_moves.size.positive?
-    # now, to store the Board totally accurately, we need to check, if there ARE en_passent possibilities IN THEORY created by a pawn moving two squares,
-    # are there REALLY any legal en_passent moves? If not, we tell the Board to reset its en_passent possibilities after all. This precision will allow
-    # three-fold repitition to trigger correctly 
+    # mate_or_mate will declare stalemate or checkmate when there are no legal moves
+
+    # now, to store the Board totally accurately, we need to check, if there ARE en_passent
+    # possibilities IN THEORY created by a pawn moving two squares,
+    # are there REALLY any legal en_passent moves? If not, we tell the Board
+    # to reset its en_passent possibilities after all. This precision will allow
+    # three-fold repitition to trigger correctly
     if board.any_en_passent_in_theory?
-      board.reset_en_passent unless legal_moves.any? { |move| move.en_passent }
+      board.reset_en_passent unless legal_moves.any?(&:en_passent)
     end
     boolean ? result.reset_moves_count : result.increase_moves_count
     result.declare_fifty_move_draw(moving_name, not_moving_name) if result.fifty_move_rule_draw?
-    
+
     result.wipe_previous_positions if boolean
     result.add_position(board.store_position)
     result.declare_repitition_draw(moving_name, not_moving_name) if result.repitition_draw?
@@ -116,28 +126,23 @@ include Miscellaneous
 
   def save_the_game
     self.saved = true
-    player_name = get_player_name_from_colour(colour_moving)
-    puts "#{player_name}, please choose a name for the saved game."
+    puts "#{moving_name}, please choose a name for the saved game."
     Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
     Dir.chdir('saved_games')
-    name = get_save_name
+    name = make_save_name
     saved_game_as_yaml = YAML.dump(self)
     file_for_saving = File.new("#{name}.txt", 'w')
     file_for_saving.puts saved_game_as_yaml
     file_for_saving.close
     puts "Game saved in 'saved_games/#{name}.txt"
     Dir.chdir('..')
-    # require input to name the saved game
-    # check if that name is already used (input needs to be validated)
-    # then save the Board, Result, Player classes (any others) to chosen_name.txt in saved_games directory, creating
-    # the directory if necessary
   end
 
-  def get_save_name
+  def make_save_name
     name_tried = gets
     if already_used?(name_tried)
-      puts "There is already a saved game with that name. Please choose another."
-      return get_save_name
+      puts 'There is already a saved game with that name. Please choose another.'
+      return make_save_name
     end
     name_tried
   end
@@ -145,7 +150,7 @@ include Miscellaneous
   def already_used?(name)
     File.exist?("#{name}.txt")
   end
-   
+
   def create_the_players
     puts 'Please input the name of the player with the White pieces. Alternatively, enter "C" for a computer player.'
     input = gets.strip
@@ -168,7 +173,4 @@ include Miscellaneous
   def mate_or_mate(check_status, result_object)
     check_status ? result_object.declare_checkmate(moving_name, not_moving_name) : result_object.declare_stalemate(moving_name, not_moving_name)
   end
-
 end
-
-
