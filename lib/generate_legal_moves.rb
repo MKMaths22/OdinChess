d76d0b2746caf08e0f81a1e7ccc5d889a28d9f29
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-07-25 16:20:48 +0100

    Minor style changes to GenerateLegalMoves

--- lib/generate_legal_moves.rb
+++ lib/generate_legal_moves.rb
@@ -2,7 +2,6 @@
 
 # GenerateLegalMoves class is responsible for finding all of the legal moves in a given position.
 class GenerateLegalMoves
-
   include Miscellaneous
 
   attr_accessor :board, :colour_moving
@@ -12,9 +11,9 @@ class GenerateLegalMoves
     @colour_moving = board.colour_moving
     # The board.colour_moving is the colour of the player whose turn it is to move
   end
-  
+
   def find_all_legal_moves
-    # outputs either an array of Move objects, which contains just one if get_just_one is true, or no items if there are no legal moves
+    # outputs an array of Move objects
     output = []
     hash_from_board = board.next_square_with_piece_to_move([-1, 7])
     # starting from [-1, 7] makes [0, 0] the next square, so it works out!
@@ -26,5 +25,4 @@ class GenerateLegalMoves
     end
     output
   end
-
 end
