884af9164e1cb8be6666ae2585161e0f3065ab61
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-07-25 11:16:16 +0100

    Remove redundant instance variables from CheckForCheck

--- lib/check_for_check.rb
+++ lib/check_for_check.rb
@@ -3,26 +3,23 @@
 # CheckForCheck class monitors whether either King is in check (to see if stalemate/checkmate when
 # no legal moves.) Also used by the Move class to verify legality re. check issues.
 class CheckForCheck
-
 include Miscellaneous
 
-  attr_accessor :colour, :poss_board_array, :king_square, :checking_piece_square, :squares_between
-  
+  attr_accessor :colour, :poss_board_array, :king_square
+
   def initialize(poss_board_array, colour)
     @colour = colour
     # colour is the colour of the King we are asking about
     @hypoth_board = Board.new(poss_board_array, other_colour(colour))
     # we want to ask whether if it is the other side to move, could they capture the King, neglecting check issues on their own King?
     @king_square = nil
-    @checking_piece_square = nil
-    @squares_between = []
   end
 
   def king_in_check?
     self.king_square = find_our_king
     current_square = [-1, 7]
     hash = @hypoth_board.next_square_with_piece_to_move(current_square)
-    # the pieces being found have the opposite colour to our king
+    # the pieces being found have the opposite colour to our king.
     while hash
       return true if hash['piece'].capture_possible?(hash['square'], @king_square, @hypoth_board)
       hash = @hypoth_board.next_square_with_piece_to_move(hash['square'])
@@ -32,7 +29,7 @@ include Miscellaneous
 
   def find_our_king
     current_square = [-1, 7]
-    # ensures that the next_square is [0, 0] to start the search
+    # ensures that the next_square is [0, 0] to start the search.
     found = false
     until found
       current_square = next_square(current_square)
