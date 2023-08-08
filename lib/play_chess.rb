6ec20fcc8363e57a845224265e06ce0df8c21f80
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-08-08 11:56:04 +0100

    With no saved_games, add moves to play_chess to test that a quick win for White works correctly

--- lib/play_chess.rb
+++ lib/play_chess.rb
@@ -68,7 +68,7 @@ end
 bye = false
 
 until bye
-  game = Game.new
+  game = Game.new(Board.new, ['player_one', 'player_two', 'e2e4', 'e7e5', 'd1f3', 'a7a6', 'f1c4', 'h7h6', 'f3f7'])
   games_saved? ? offer_reload : game.play_game
   bye = stop_chess_boolean
 end
