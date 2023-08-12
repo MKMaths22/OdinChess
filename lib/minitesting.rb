a9ed6b2bda25d8fa60657ca72a8b5363b7055c66
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-08-12 13:34:40 +0100

    Remove single-move test and document illegal moves in #test_long_game_illegal_moves

--- lib/minitesting.rb
+++ lib/minitesting.rb
@@ -92,14 +92,6 @@ class ChessTest < Minitest::Test
     assert test_game.result.game_end_message == "It's a draw by stalemate! Well played, player_one and player_two."
   end
 
-  def test_first_move_only_not_real_test
-    # skip
-    list_of_inputs = ['player_one', 'player_two', 'e2e4']
-    test_game = Game.new(Board.new, list_of_inputs)
-    test_game.play_game
-    refute test_game.result.game_over?
-  end
-
   def test_illegal_move_counted
      # skip
      list_of_inputs = ['player_one', 'player_two', 'e2e4', 'e7d4']
@@ -117,6 +109,26 @@ class ChessTest < Minitest::Test
   end
 
   def test_long_game_illegal_moves
+    # this tests a lot of illegal move types in one go!
+    # Move 1 is 'd2e3' --- a pawn attempting to capture on an empty square (no en_passent)
+    # Move 3 is 'f8c5' --- bishop tries to jump over a pawn
+    # Move 7 is 'd4d5' --- pawn tries to capture straight ahead
+    # Move 8 is 'c3c5' --- pawn tries to go 2 squares after its first move
+    # Move 9 is 'a1a7' --- rook tries to jump over a pawn
+    # Move 13 is 'c7c5' --- fails to block check from queen
+    # Move 15 is 'e1c1' --- attempt to castle with knight in the way
+    # Move 17 is 'c6c5' --- pawn move would discover check on own king from queen
+    # Move 20 is 'b6b1' --- queen attempts to jump over pawn
+    # Move 22 is 'e1c1' --- attempt to castle into check
+    # Move 25 is 'e1c1' --- attempt to castle passing through check
+    # Move 28 is 'e1c1' --- attempt to castle from check
+    # Move 31 is 'e5d6' --- attempt to en_passent not made immediately after d7-d5,
+    # followed by genuine en_passent capture on f6 :-)
+    # Move 40 is 'e1c1' --- attempt to castle with king having moved (and returned to start square)
+    # Move 46 is 'e8c8' --- attempt to castle with no rook
+    # Move 57 is 'h8g8' --- moves king towards checking queen
+    # the game ends with discovered double check, so that check is detected not just from
+    # the piece that moved.
     # skip
     list_of_inputs = ['Peter', 'Chris', 'd2e3', 'c2c3', 'f8c5', 'g8h6', 'd2d4', 'd7d5', 'd4d5', 'c3c5', 'a1a7', 'c1h6', 'g7h6', 'd1a4', 'c7c5', 'c7c6', 'e1c1', 'b1a3', 'c6c5', 'd8b6', 'e2e4', 'b6b1', 'b6b2', 'e1c1', 'e4e5', 'b2b3', 'e1c1', 'h2h4', 'b3c3', 'e1c1', 'e1e2', 'f7f5', 'e5d6', 'e5f6', 'b8d7', 'a4a7', 'c3d4', 'e2e1', 'e7e5', 'a7a8', 'd4h4', 'e1c1', 'f1a6', 'd7c5', 'a8b7', 'c8b7', 'g2g3', 'e8c8', 'f8d6', 'f2f3', 'e8g8', 'h1h4', 'g8h8', 'a6b7', 'c5a4', 'f6f7', 'f8e8', 'f7e8', 'Q', 'h8g8', 'h8g7', 'h4f4', 'e5e4', 'f4f6', 'e4e3', 'e8e5', 'g7h8', 'f6f8']
     test_game = Game.new(Board.new, list_of_inputs)
